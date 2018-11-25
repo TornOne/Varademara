ac689fb2ba2bd941ccf5ed9061457088d7d23ea1
Helen Õunapuu <helen.ounapuu@gmail.com>
2018-11-25 17:19:58 +0200

    UI update

---
+++ Assets/Scripts/UI/SidebarInformation.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SidebarInformation : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void fillSidebar(Unit unit){
+        print(unit.pAtt);
+    }
+}
