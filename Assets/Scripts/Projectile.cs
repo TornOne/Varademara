4275ada87b402c465706b1ff1f3f75d4b459e958
TornOne <ott.adermann@gmail.com>
2019-01-17 20:37:13 +0200

    Projectile script

---
+++ Assets/Scripts/Projectile.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour {
+	public Vector3 destination;
+	public float speed;
+
+	void Start() {
+		StartCoroutine(ApproachDestination());
+	}
+
+	IEnumerator ApproachDestination() {
+		Vector3 source = transform.position;
+		float startTime = Time.time;
+		float travelTime = Vector3.Distance(source, destination) / speed;
+		while (Time.time < startTime + travelTime) {
+			transform.position = Vector3.Lerp(source, destination, (Time.time - startTime) / travelTime);
+			yield return null;
+		}
+		transform.position = destination;
+	}
+}
